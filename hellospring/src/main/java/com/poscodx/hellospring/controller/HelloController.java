ec9932d2303a14852c2834638bee5a0ea27d12fd
YeJi222 <hhongyizi@gmail.com>
2023-09-14 14:34:52 +0900

    etc: git test

---
+++ hellospring/src/main/java/com/poscodx/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.poscodx.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/hello")
+	public String hello() {
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+	public void test(){
+
+	}
+}
+
+
+/*
+	경로 : http://localhost:8080/hellospring/hello
+*/
