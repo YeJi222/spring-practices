bf42f5fd51335ed9cb581a3c7cf19e70cd710658
YeJi222 <hhongyizi@gmail.com>
2023-09-15 14:24:38 +0900

    feat: lecture-emaillist03 #2

--- emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/poscodx/emaillist/controller/EmaillistController.java
@@ -1,21 +1,47 @@
 package com.poscodx.emaillist.controller;
 
+import com.poscodx.emaillist.vo.EmaillistVo;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.ui.Model;
 import com.poscodx.emaillist.repository.EmaillistRepository;
 
 @Controller
 public class EmaillistController {
 	@Autowired
 	private EmaillistRepository emaillistRepository;
-	
+
 	@ResponseBody
 	@RequestMapping("/")
 	public String main() {
-		// emaillistRepository.findAll();
+		System.out.println("test");
 		return "ok!";
 	}
+
+	/*
+	@RequestMapping("/")
+	public String main(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+
+		return "main";
+	}
+	 */
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add(){
+		System.out.println("test");
+		return "add";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(EmaillistVo vo){
+		emaillistRepository.insert(vo);
+		return "redirect:/";
+	}
+
 }
