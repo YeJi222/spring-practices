45a748e2f193beed2b8b288af4a0f10bb44fc5dc
YeJi222 <hhongyizi@gmail.com>
2023-10-04 10:52:59 +0900

    feat: lecture-container#2 : user

---
+++ container/src/main/java/com/poscodx/container/user/User.java
@@ -0,0 +1,14 @@
+package com.poscodx.container.user;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+	private String name = "둘리";
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+}
