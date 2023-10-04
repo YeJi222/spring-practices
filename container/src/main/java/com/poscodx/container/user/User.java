6ca31e762219f7aea4d2c21d10a69fc76e846415
YeJi222 <hhongyizi@gmail.com>
2023-10-04 11:30:15 +0900

    feat: lecture-create bean(up to using setter)

--- container/src/main/java/com/poscodx/container/user/User.java
+++ container/src/main/java/com/poscodx/container/user/User.java
@@ -4,11 +4,35 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class User {
+	private Long no = 0L;
 	private String name = "둘리";
 	
+	public User() {
+	}
+	
+	public User(String name) {
+		this.name = name;
+	}
+	
+	public User(Long no, String name) {
+		this.no = no;
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
 	}
 	
+	public void setNo(Long no) {
+		this.no = no;
+	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [no=" + no + ", name=" + name + "]";
+	}
 }
